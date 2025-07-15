bd5f4c2c0f78e8aa3617b4960ddb780500367954
root <raflymg3@gmail.com>
2025-07-15 15:01:40 +0000

    test webhook 200

--- webhook.js
+++ webhook.js
@@ -8,21 +8,28 @@ const secret = '220102'; // samakan dengan secret di GitHub
 
 app.use(bodyParser.json());
 
+//app.post('/webhook', (req, res) => {
+//    const signature = req.headers['x-hub-signature-256'];
+//    const hmac = crypto.createHmac('sha256', secret);
+//    const digest = 'sha256=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+
+//    if (signature === digest) {
+//        exec('/var/www/helloapp/deploy.sh', (err, stdout, stderr) => {
+//            console.log(stdout, stderr);
+//        });
+//        res.sendStatus(200);
+//    } else {
+//        res.sendStatus(403);
+//    }
+//});
+
 app.post('/webhook', (req, res) => {
-    const signature = req.headers['x-hub-signature-256'];
-    const hmac = crypto.createHmac('sha256', secret);
-    const digest = 'sha256=' + hmac.update(JSON.stringify(req.body)).digest('hex');
-
-    if (signature === digest) {
-        exec('/var/www/helloapp/deploy.sh', (err, stdout, stderr) => {
-            console.log(stdout, stderr);
-        });
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(403);
-    }
+    console.log("Webhook received!");
+    exec('/var/www/helloapp/deploy.sh', (err, stdout, stderr) => {
+        console.log(stdout, stderr);
+    });
+    res.sendStatus(200);
 });
-
 app.listen(9000, () => {
     console.log('Webhook listener running on port 9000');
 });
